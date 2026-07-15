ec8afee502786956adb2684dc8c99fb154164e4c
Patrick Hampson <mphampson@gmail.com>
2026-07-15 14:11:52 -0600

    card-scanner: create _headers frame-ancestors + cache v2

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'card-scanner-v1'
+const CACHE = 'card-scanner-v2'
 self.addEventListener('install', (e) => { self.skipWaiting() })
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then((keys) => Promise.all(
